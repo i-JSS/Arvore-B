c13b9f6841581bc8543d52c75c0688731506e8a2
Hunter104 <000plagueinc@gmail.com>
2025-06-16 18:40:26 -0300

    Upload de nó e invariante

--- src/main.py
+++ src/main.py
@@ -1,3 +1,15 @@
 import icontract
 
+@icontract.invariant(lambda self: all(self.keys[i] <= self.keys[i + 1] for i in range(len(self.keys) - 1)))
+class Node:
+
+    def __init__(self, t: int, n: int, leaf: bool):
+        self.t = t
+        self.n = n
+        self.leaf = leaf
+        self.keys = []
+        self.children = []
+
+
+
 print("TOTOLA")
