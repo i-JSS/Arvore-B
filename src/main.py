36ae1062f64c09ce5dd19777734bf5e54c85b06c
Hunter104 <000plagueinc@gmail.com>
2025-06-19 15:54:17 -0300

    Conserta módulo main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import icontract
-from tree import Tree
+from btree import BTree
 
 if __name__ == "__main__":
-    tree = Tree(2)
+    tree = BTree(2)
     for k in [10, 20, 5, 6, 12, 30, 7, 17, 21, 22, 34, 23, 24, 35]:
         tree.print_tree()
         tree.insert(k)
