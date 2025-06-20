7953d3f789397e96c3376bf2817b497183539883
heylisten64 <gabriela.leemoss@gmail.com>
2025-06-20 09:36:25 -0300

    adicionando busca

--- src/btree.py
+++ src/btree.py
@@ -18,14 +18,14 @@ class BTree:
     def height(self):
         return self.root.height()
 
-    # @icontract.require(lambda self: self.CHAVE INSERIDA NAO EXISTE NA ARVORE)
+    @icontract.require(lambda self, key: not self.search(key), "Chave a ser inserida não deve existir na árvore")
     @icontract.ensure(lambda self: self.root.subtree_valid())
     def insert(self, key):
         if self.root.is_full:
             self._grow_tree()
         self.root.insert_non_full(key)
 
-    # @icontract.require(lambda self: self.CHAVE A SER REMOVIDA EXISTE NA ARVORE)
+    @icontract.require(lambda self, key: self.search(key), "Chave a ser removida deve existir na árvore")
     @icontract.ensure(lambda self: self.root.subtree_valid())
     def remove(self, key):
         pass
@@ -45,3 +45,19 @@ class BTree:
 
     def print_tree(self):
         self.root.print_tree()
+
+    def search(self, key: int, node: Node = None) -> bool:
+        if node is None:
+            node = self.root
+
+        i = 0
+        while i < len(node.keys) and key > node.keys[i]:
+            i += 1
+
+        if i < len(node.keys) and key == node.keys[i]:
+            return True
+
+        if node.is_leaf:
+            return False
+
+        return self.search(key, node.children[i])
